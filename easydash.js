a49a98bbaab40994fb63091f33b7d3ec27f5c7b4
Dipen Modi <clamstudios0905@gmail.com>
2023-12-18 06:17:54 +0530

    fix: some updates

--- easydash.js
+++ easydash.js
@@ -2,30 +2,30 @@ import { resolve } from 'path';
 import * as fs from 'fs';
 
 export default function multipagePlugin() {
-  return {
-    name: 'easydash',
-    configResolved(config) {
-      const pagesDir = resolve(__dirname, 'pages');
-      const entries = {};
+	return {
+		name: 'easydash',
+		configResolved(config) {
+			const pagesDir = resolve(__dirname, 'pages');
+			const entries = {};
 
-      const scanDirectory = (dir) => {
-        const files = fs.readdirSync(dir);
-        for (const file of files) {
-          const filePath = resolve(dir, file);
-          if (fs.statSync(filePath).isFile() && file.endsWith('.html')) {
-            entries[filePath] = filePath; // Use original filename by default
-          } else if (fs.statSync(filePath).isDirectory()) {
-            scanDirectory(filePath); // Recursively scan subdirectories
-          }
-        }
-      };
+			const scanDirectory = (dir) => {
+				const files = fs.readdirSync(dir);
+				for (const file of files) {
+					const filePath = resolve(dir, file);
+					if (fs.statSync(filePath).isFile() && file.endsWith('.html')) {
+						entries[filePath] = filePath; // Use original filename by default
+					} else if (fs.statSync(filePath).isDirectory()) {
+						scanDirectory(filePath); // Recursively scan subdirectories
+					}
+				}
+			};
 
-      scanDirectory(pagesDir);
+			scanDirectory(pagesDir);
 
-      config.build.rollupOptions.input = {
-        ...config.build.rollupOptions.input,
-        ...entries,
-      };
-    },
-  };
+			config.build.rollupOptions.input = {
+				...config.build.rollupOptions.input,
+				...entries,
+			};
+		},
+	};
 }
--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,19 @@ import { resolve } from 'path';
 import multipagePlugin from './easydash';
 
 export default defineConfig({
-  plugins: [multipagePlugin()],
-  build: {
-    rollupOptions: {
-      // defaultInterop: "esnext", // Prefer ES modules
-      input: {
-        main: resolve(__dirname, 'index.html'), // Main entry point
-      },
-      output: {
-        entryFileNames: '*.html', // Output filename pattern with placeholder
-      },
-    },
-  },
-  server: {
-    host: true,
-  }
+	plugins: [multipagePlugin()],
+	build: {
+		rollupOptions: {
+			// defaultInterop: "esnext", // Prefer ES modules
+			input: {
+				main: resolve(__dirname, 'index.html'), // Main entry point
+			},
+			output: {
+				entryFileNames: '*.html', // Output filename pattern with placeholder
+			},
+		},
+	},
+	server: {
+		host: true,
+	}
 });
